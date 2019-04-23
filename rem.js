95a57353fc6bc91bfb53bae53f55d818640f9e59
pioneers01 <125928721@qq.com>
2019-04-23 18:29:14 +0800

    two

--- rem.js
+++ rem.js
@@ -5,7 +5,7 @@
         const ua = navigator.userAgent;
         const matches = ua.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i);
         const isIos = navigator.appVersion.match(/(iphone|ipad|ipod)/gi);
-        const dpr = window.devicePixelRatio || 1;
+        let dpr = window.devicePixelRatio || 1;
         if (!isIos && !(matches && matches[1] > 534)) {
             // 如果非iOS, 非Android4.3以上, dpr设为1;
             dpr = 1;
@@ -18,7 +18,6 @@
             window.document.head.appendChild(metaEl);
         }
         metaEl.setAttribute('content', 'width=device-width,user-scalable=no,initial-scale=' + scale + ',maximum-scale=' + scale + ',minimum-scale=' + scale);
-    
         document.documentElement.style.fontSize = document.documentElement.clientWidth / (750 / _baseFontSize) + 'px';
     };
     recalc();
